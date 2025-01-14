1a2befc3a463255930309f9fa91a53c64b6daa5e
VastSea0 <vastseaoffical0@outlook.com>
2025-01-15 00:10:03 +0300

    Add WeatherUI page implementation with UI elements for weather display

---
+++ lib/pages/hava_durumu.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class WeatherUI extends StatefulWidget {
+  const WeatherUI({super.key, required this.title});
+  final String title;
+
+  @override
+  _WeatherUIState createState() => _WeatherUIState();
+}
+
+class _WeatherUIState extends State<WeatherUI> {
+  void test() {
+    setState(() {
+      print("Test");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.only(top: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Theme.of(context).colorScheme.primaryContainer,
+              Theme.of(context).colorScheme.tertiaryContainer,
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child:
+                      const Icon(Icons.cloud, size: 200, color: Colors.white),
+                ),
+                Text(
+                  "Istanbul",
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      // Arka Plan
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Colors.blue.withOpacity(0.4),
+                              Colors.purple.withOpacity(0.4),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // Glassmorphism Efekti
+                      Center(
+                        child: ClipRRect(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Container(
+                              padding: const EdgeInsets.all(20),
+                              width: double.infinity,
+                              height:
+                                  MediaQuery.sizeOf(context).height - 276 - 37,
+                              decoration: BoxDecoration(
+                                color: Colors.white
+                                    .withOpacity(0.3), // Yarı saydam arka plan
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                ),
+                                border: Border.all(
+                                  color: Colors.white.withOpacity(0.4),
+                                  width: 1.5,
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.1),
+                                    blurRadius: 10,
+                                    offset: Offset(0, 4),
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(15),
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(16)),
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: const [
+                                            Icon(Icons.location_on,
+                                                color: Colors.black),
+                                            SizedBox(width: 8),
+                                            Text(
+                                              "Country: Turkiye",
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                            height: 10), // Araya boşluk ekler
+                                        Row(
+                                          children: const [
+                                            Icon(Icons.thermostat,
+                                                color: Colors.black),
+                                            SizedBox(width: 8),
+                                            Text(
+                                              "Temperature: 25°C",
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 10),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.water_drop,
+                                              color: Colors.black,
+                                            ),
+                                            SizedBox(width: 8),
+                                            Text(
+                                              "Humidity: 73%",
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Container(
+                                          width: double.infinity,
+                                          height: 150,
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              TextField(
+                                                  decoration: InputDecoration(
+                                                    border:
+                                                        OutlineInputBorder(),
+                                                    labelText:
+                                                        "Enter Your Country",
+                                                  ),
+                                                  style: TextStyle(
+                                                      color: Colors
+                                                          .black // TextField içine yazılan metin rengi
+                                                      )),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
